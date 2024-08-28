f4face05a2707e96f79edf0938c41377d4f1c822
stanabaz <anas.abazid@student.hs-anhalt.de>
2024-08-29 01:22:54 +0200

    Audio Player

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,8 +1,44 @@
+import javax.sound.sampled.*;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args)
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException
     {
-        
+        Scanner scanner = new Scanner(System.in);
+        File file = new File("Waiting-Andrew-Langdon.wav");
+        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioInputStream);
+
+        String response = "";
+        while (!response.equals("Q"))
+        {
+            System.out.println("P = play, S = Stop, R = Reset, Q = Quit");
+            System.out.println("Enter your choice: ");
+
+            response = scanner.next();
+            response = response.toUpperCase();
+
+            switch (response)
+            {
+                case ("P"): clip.start();
+                break;
+                case ("S"): clip.stop();
+                break;
+                case ("R"): clip.setMicrosecondPosition(0);
+                break;
+                case ("Q"): clip.close();
+                break;
+                default: System.out.println("Not a valid response!");
+            }
+        }
+        System.out.println("✌️");
     }
 }
