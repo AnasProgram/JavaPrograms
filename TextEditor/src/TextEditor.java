6285a6d366fc93def4e51ce1d571181017f8433e
stanabaz <anas.abazid@student.hs-anhalt.de>
2024-08-28 17:48:15 +0200

    a text editor app

--- TextEditor/src/TextEditor.java
+++ TextEditor/src/TextEditor.java
@@ -1,9 +1,14 @@
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class TextEditor extends JFrame implements ActionListener
 {
@@ -13,6 +18,12 @@ public class TextEditor extends JFrame implements ActionListener
     JLabel jLabel;
     JButton jButton;
     JComboBox jComboBox;
+    JMenuBar menuBar;
+    JMenu menu;
+    JMenuItem openItem;
+    JMenuItem saveItem;
+    JMenuItem exitItem;
+
     TextEditor()
     {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -51,7 +62,26 @@ public class TextEditor extends JFrame implements ActionListener
         jComboBox.addActionListener(this);
         jComboBox.setSelectedItem("Arial");
 
+        //---------------menuBar
+        menuBar = new JMenuBar();
+        menu = new JMenu("File");
+        openItem = new JMenuItem("Open");
+        saveItem = new JMenuItem("Save");
+        exitItem = new JMenuItem("Exit");
 
+        openItem.addActionListener(this);
+        saveItem.addActionListener(this);
+        exitItem.addActionListener(this);
+
+        menu.add(openItem);
+        menu.add(saveItem);
+        menu.add(exitItem);
+
+        menuBar.add(menu);
+
+        //--------------/menubar
+
+        this.setJMenuBar(menuBar);
         this.add(jLabel);
         this.add(jSpinner);
         this.add(jButton);
@@ -72,6 +102,70 @@ public class TextEditor extends JFrame implements ActionListener
         if (event.getSource() == jComboBox)
         {
             textArea.setFont(new Font((String) jComboBox.getSelectedItem(), Font.PLAIN, textArea.getFont().getSize()));
+        }
+        if (event.getSource() == openItem)
+        {
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setCurrentDirectory(new File("."));
+            FileNameExtensionFilter fileNameExtensionFilter = new FileNameExtensionFilter("Text files", "txt");
+            fileChooser.setFileFilter(fileNameExtensionFilter);
+
+            int response = fileChooser.showOpenDialog(null);
+
+            if(response == JFileChooser.APPROVE_OPTION)
+            {
+                File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                Scanner scannerfileIn = null;
+                try
+                {
+                    scannerfileIn = new Scanner(file);
+                    if (file.isFile())
+                    {
+                        while (scannerfileIn.hasNextLine())
+                        {
+                            String line = scannerfileIn.nextLine()+"\n";
+                            textArea.append(line);
+                        }
+                    }
+                }
+                catch (FileNotFoundException e)
+                {
+                    throw new RuntimeException(e);
+                }
+                finally
+                {
+                    scannerfileIn.close();
+                }
+            }
+        }
+        if (event.getSource() == saveItem)
+        {
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setCurrentDirectory(new File("."));
+
+            int response = fileChooser.showSaveDialog(null);
+            if(response == JFileChooser.APPROVE_OPTION)
+            {
+                File file;
+                PrintWriter fileOut = null;
+                file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                try {
+                    fileOut = new PrintWriter(file);
+                    fileOut.println(textArea.getText());
+
+                } catch (FileNotFoundException e)
+                {
+                    throw new RuntimeException(e);
+                }
+                finally
+                {
+                    fileOut.close();
+                }
+            }
+        }
+        if (event.getSource() == exitItem)
+        {
+            System.exit(0);
         }
     }
 }
