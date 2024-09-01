07b2eb7854f902561d4d6aa8b732cf869ab778de
stanabaz <anas.abazid@student.hs-anhalt.de>
2024-09-01 22:07:04 +0200

    easy way to choose a file with my code which I learned today!

---
+++ Java select a file/src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // JFileChooser = A GUI mechanism that let's a user choose a file (helpful for opening or saving files)
+        new MyFrame();
+    }
+}
