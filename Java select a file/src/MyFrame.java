239811f017e5834cf9687f6d3a18212390c0e10a
stanabaz <anas.abazid@student.hs-anhalt.de>
2024-09-01 22:10:06 +0200

    update

--- Java select a file/src/MyFrame.java
+++ Java select a file/src/MyFrame.java
@@ -21,13 +21,13 @@ public class MyFrame extends JFrame implements ActionListener
     public void actionPerformed(ActionEvent e)
     {
         if(e.getSource()==button)
-        {            JFileChooser fileChooser = new JFileChooser();
+        {
+            JFileChooser fileChooser = new JFileChooser();
 
             fileChooser.setCurrentDirectory(new File(".")); //sets current directory
 
             int response = fileChooser.showOpenDialog(null); //select file to open
             //int response = fileChooser.showSaveDialog(null); //select file to save
-
             if(response == JFileChooser.APPROVE_OPTION)
             {
                 File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
