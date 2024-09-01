52bc51746940ee226e18f3672cc72b1ac0b84c60
stanabaz <anas.abazid@student.hs-anhalt.de>
2024-09-01 16:03:46 +0200

    I've learned how to create a simple combobox which means box for selecting an aim!

---
+++ Java combobox/src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // JComboBox = A component that combines a button or editable field and a drop-down list
+        new MyFrame();
+    }
+}
